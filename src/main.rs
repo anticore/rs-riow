0d4a43aa07b0e48823a60c0ab75b25269ec8fbdd
anticore <slimedev@gmail.com>
2020-06-21 23:29:45 +0100

    ppm image creation code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use std::fs::File;
+use std::fs::OpenOptions;
+use std::path::Path;
+use std::io::prelude::*;
+
+fn create_image() {
+    let path = Path::new("image.ppm");
+    let display = path.display();
+
+    match File::create(&path) {
+        Err(why) => panic!("could not create {}: {}", display, why),
+        Ok(file) => file
+    };
+}
+
+fn write_to_image(line: String) {
+    let mut file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .append(true)
+        .open("image.ppm")
+        .unwrap();
+
+    if let Err(why) = writeln!(file, "{}", line) {
+        panic!("couldn't write to file: {}", why);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    create_image();
+
+    write_to_image(String::from("P3\n256 256\n255\n"));
+
+    let mut color: String;
+    for i in 0..256 {
+        println!("line {}", i);
+        for j in 0..256 {
+            color = format!("{} {} 0", i, j);
+            write_to_image(color);
+        }
+    }
 }
