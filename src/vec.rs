7eacc2bc433e6dbb6170dc15e4f02a2a3d4a6a7e
anticore <slimedev@gmail.com>
2020-06-22 18:02:22 +0100

    completed vector code

--- src/vec.rs
+++ src/vec.rs
@@ -2,6 +2,7 @@
 
 use std::ops;
 
+/// 3d vector
 pub struct Vec3 {
     pub x: f64,
     pub y: f64,
@@ -22,6 +23,7 @@ impl Vec3 {
 impl ops::Add<&Vec3> for &Vec3 {
     type Output = Vec3;
 
+    #[inline]
     fn add(self, other: &Vec3) -> Vec3 {
         Vec3 { x: self.x + other.x, y: self.y + other.y, z: self.z + other.z }
     }
@@ -31,7 +33,65 @@ impl ops::Add<&Vec3> for &Vec3 {
 impl ops::Add<f64> for &Vec3 {
     type Output = Vec3;
 
+    #[inline]
     fn add(self, other: f64) -> Vec3 {
         Vec3 { x: self.x + other, y: self.y + other, z: self.z + other }
+    }
+}
+
+/// vector * vector
+impl ops::Mul<&Vec3> for &Vec3 {
+    type Output = Vec3;
+
+    #[inline]
+    fn mul(self, other: &Vec3) -> Vec3 {
+        Vec3 { x: self.x * other.x, y: self.y * other.y, z: self.z * other.z }
+    }
+}
+
+/// vector * scalar
+impl ops::Mul<f64> for &Vec3 {
+    type Output = Vec3;
+
+    #[inline]
+    fn mul(self, other: f64) -> Vec3 {
+        Vec3 { x: self.x * other, y: self.y * other, z: self.z * other }
+    }
+}
+
+/// scalar * vector
+impl ops::Mul<&Vec3> for f64 {
+    type Output = Vec3;
+
+    #[inline]
+    fn mul(self, other: &Vec3) -> Vec3 {
+        Vec3 { x: self * other.x, y: self * other.y, z: self * other.z }
+    }
+}
+
+/// vector / scalar
+impl ops::Div<f64> for &Vec3 {
+    type Output = Vec3;
+
+    #[inline]
+    fn div(self, other: f64) -> Vec3 {
+        return (1. / other) * self;
+    }
+}
+
+/// dot product
+#[inline]
+pub fn dot(v1: &Vec3, v2: &Vec3) -> f64 {
+    return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+}
+
+
+/// cross product
+#[inline]
+pub fn cross(v1: &Vec3, v2: &Vec3) -> Vec3 {
+    Vec3 { 
+        x: v1.y * v2.z - v1.z * v2.y, 
+        y: v1.z * v2.x - v1.x * v2.z, 
+        z: v1.x * v2.y - v1.y * v2.x 
     }
 }
