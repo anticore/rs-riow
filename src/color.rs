4b2deeb779305c7deaa2ca45e3e5e49a88e0dbb5
anticore <slimedev@gmail.com>
2020-06-27 21:01:37 +0100

    clear color code; add random functions and rnd! macro

--- src/color.rs
+++ src/color.rs
@@ -10,21 +10,14 @@ pub struct Color {
     pub a: f32
 }
 
-fn clamp (n: f32) -> f32 {
-    //if n < 0. { return 0. }
-    //else if n > 1. { return 1. }
-    //else { return n };
-    return n;
-}
-
 impl Color {
     pub fn new(r: f32, g: f32, b: f32, a: f32) -> Color {
 
         Color { 
-            r: clamp(r), 
-            g: clamp(g), 
-            b: clamp(b), 
-            a: clamp(a) 
+            r, 
+            g, 
+            b, 
+            a
         }
     }
 }
@@ -35,10 +28,10 @@ impl ops::Add<Color> for Color {
     #[inline]
     fn add(self, other: Color) -> Color {
         Color { 
-            r: clamp(&self.r + &other.r), 
-            g: clamp(&self.g + &other.g), 
-            b: clamp(&self.b + &other.b), 
-            a: clamp(&self.a + &other.a) 
+            r: self.r + other.r, 
+            g: self.g + other.g, 
+            b: self.b + other.b, 
+            a: self.a + other.a 
         }
     }
 }
@@ -49,10 +42,10 @@ impl ops::Add<f32> for Color {
     #[inline]
     fn add(self, other: f32) -> Color {
         Color { 
-            r: clamp(&self.r + other), 
-            g: clamp(&self.g + other), 
-            b: clamp(&self.b + other), 
-            a: clamp(&self.a + other) 
+            r: self.r + other, 
+            g: self.g + other, 
+            b: self.b + other, 
+            a: self.a + other 
         }
     }
 }
@@ -63,10 +56,10 @@ impl ops::Sub<Color> for Color {
     #[inline]
     fn sub(self, other: Color) -> Color {
         Color { 
-            r: clamp(&self.r - &other.r), 
-            g: clamp(&self.g - &other.g), 
-            b: clamp(&self.b - &other.b), 
-            a: clamp(&self.a - &other.a)
+            r: self.r - &other.r, 
+            g: self.g - &other.g, 
+            b: self.b - &other.b, 
+            a: self.a - &other.a
         }
     }
 }
@@ -77,10 +70,10 @@ impl ops::Sub<f32> for Color {
     #[inline]
     fn sub(self, other: f32) -> Color {
         Color { 
-            r: clamp(&self.r - other), 
-            g: clamp(&self.g - other), 
-            b: clamp(&self.b - other), 
-            a: clamp(&self.a - other) 
+            r: self.r - other, 
+            g: self.g - other, 
+            b: self.b - other, 
+            a: self.a - other 
         }
     }
 }
@@ -91,10 +84,10 @@ impl ops::Mul<Color> for Color {
     #[inline]
     fn mul(self, other: Color) -> Color {
         Color { 
-            r: clamp(&self.r * &other.r), 
-            g: clamp(&self.g * &other.g),
-            b: clamp(&self.b * &other.b), 
-            a: clamp(&self.a * &other.a) 
+            r: self.r * &other.r, 
+            g: self.g * &other.g,
+            b: self.b * &other.b, 
+            a: self.a * &other.a 
         }
     }
 }
@@ -105,10 +98,10 @@ impl ops::Mul<f32> for Color {
     #[inline]
     fn mul(self, other: f32) -> Color {
         Color { 
-            r: clamp(&self.r * other), 
-            g: clamp(&self.g * other), 
-            b: clamp(&self.b * other), 
-            a: clamp(&self.a * other) 
+            r: self.r * other, 
+            g: self.g * other, 
+            b: self.b * other, 
+            a: self.a * other 
         }
     }
 }
@@ -119,10 +112,10 @@ impl ops::Mul<Color> for f32 {
     #[inline]
     fn mul(self, other: Color) -> Color {
         Color { 
-            r: clamp(self * &other.r), 
-            g: clamp(self * &other.g), 
-            b: clamp(self * &other.b), 
-            a: clamp(self * &other.a) 
+            r: self * &other.r, 
+            g: self * &other.g, 
+            b: self * &other.b, 
+            a: self * &other.a 
         }
     }
 }
@@ -136,22 +129,3 @@ impl ops::Div<f32> for Color {
     }
 }
 
-
-mod tests {
-    #[allow(unused_imports)]
-    use super::*;
-
-    #[test]
-    fn test_new() {
-        let color: Color = Color::new(0., 0., 0., 1.);
-        assert_eq!(color.r, 0.);
-        assert_eq!(color.g, 0.);
-        assert_eq!(color.b, 0.);
-        assert_eq!(color.a, 1.);
-
-        let color = Color::new(-1., 0.4, 2., 1.);
-        assert_eq!(color.r, 0.);
-        assert_eq!(color.g, 0.4);
-        assert_eq!(color.b, 1.);
-    }
-}
--- src/common.rs
+++ src/common.rs
@@ -1,8 +1,25 @@
 #![allow(dead_code)]
 
+use rand::Rng;
+
 pub const PI: f32 = 3.1415926535897932385;
 pub const INFINITY: f32 = f32::INFINITY;
 
 pub fn deg_to_rad(degrees: f32) -> f32 {
     return degrees * PI / 180.;
+}
+
+pub fn random_f(min: f32, max: f32) -> f32 {
+    let mut rng = rand::thread_rng();
+    return rng.gen_range(min, max);
+}
+
+#[macro_export]
+macro_rules! rnd {
+    ($a: literal, $b: literal) => {
+        random_f($a, $b)
+    };
+    () => {
+        random_f(0., 1.)
+    }
 }
